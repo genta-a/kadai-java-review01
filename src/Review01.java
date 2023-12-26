31187667cbc8c13341fc76a33408ff06526fa6e7
genta-a <gentaro-a-204@hotmail.co.jp>
2023-12-27 01:09:15 +0900

    税率の記載方法を変更、結果について小数点以下が記載されないように調整。

--- src/Review01.java
+++ src/Review01.java
@@ -1,19 +1,21 @@
 
 public class Review01 {
 
+  static double tax_rate = 0.1;//税率を設定
+
     public static void main(String[] args) {
-       int value = 500; //価格を投入
-       double result = tax (value);//消費税額を反映
-       double total = value + result;//合計額を計算
+       int price = 59500; //価格を投入
+       int result = (int) tax (price);//消費税額を反映
+       int total = price + result;//合計額を計算
 
        System.out.println
-       ((value) + "円の商品の税込み価格は" + (total) + "(消費税は" + (result) + "円)です。");
+       ((price) + "円の商品の税込み価格は" + (total) + "(消費税は" + (result) + "円)です。");
        //もともとの価格に対しての税込み額、税額を表示。
     }
 
     //taxメソッドで消費税を計算
     public static double tax(double price) {
-        double result = Math.round(price * 0.1);//価格に税率「10%」を乗算
+        double result = Math.round(price * tax_rate);//価格に税率「10%」を乗算
         return result;//mainメソッドに返す
     }
 
