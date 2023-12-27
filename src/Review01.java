f163174d775cc8d25db453683ae9fd50b8f49f59
genta-a <gentaro-a-204@hotmail.co.jp>
2023-12-27 22:52:57 +0900

    12/27再提出

--- src/Review01.java
+++ src/Review01.java
@@ -5,18 +5,18 @@ public class Review01 {
 
     public static void main(String[] args) {
        int price = 59500; //価格を投入
-       int result = (int) tax (price);//消費税額を反映
+       int result = tax (price);//消費税額を反映
        int total = price + result;//合計額を計算
 
        System.out.println
-       ((price) + "円の商品の税込み価格は" + (total) + "(消費税は" + (result) + "円)です。");
+       ((price) + "円の商品の税込価格は" + (total) + "円（消費税は" + (result) + "円）です。");
        //もともとの価格に対しての税込み額、税額を表示。
     }
 
     //taxメソッドで消費税を計算
-    public static double tax(double price) {
-        double result = Math.round(price * tax_rate);//価格に税率「10%」を乗算
-        return result;//mainメソッドに返す
+    public static int tax(int price) {
+        int result = (int) Math.round(price * tax_rate);//価格に税率「10%」を乗算
+        return (int) result;//mainメソッドに返す
     }
 
 }
